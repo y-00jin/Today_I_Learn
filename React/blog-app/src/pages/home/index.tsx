6d7802eb252f76047e0c6666cad0949e5251585e
yjlee <81798918+y-00jin@users.noreply.github.com>
2024-03-06 13:38:13 +0900

    24.03.06 React 기본 컴포넌트 생성(3)
    24.03.06 React 기본 컴포넌트 생성(3)

--- React/blog-app/src/pages/home/index.tsx
+++ React/blog-app/src/pages/home/index.tsx
@@ -12,6 +12,10 @@ export default function Home() {
                     <Link to="/profile">프로필</Link>
                 </div>
             </header>
+            <div className="post__navigation">
+                <div className="post__navigation--active">전체</div>
+                <div>나의 글</div>
+            </div>
             <div className="post__list">
                 {[...Array(10)].map((e, index) => (
                     <div key={index} className="post__box">
@@ -24,8 +28,10 @@ export default function Home() {
                             <div className="post__title">게시글 {index}</div>
                             <div className="post__text">
                                 Vivamus ante purus, efficitur a posuere tempus, feugiat vel magna. Ut euismod neque massa, quis luctus leo venenatis ac. Nunc auctor dui a scelerisque lacinia. Duis ac lectus sed libero efficitur faucibus. Maecenas condimentum leo sem, at rutrum purus tristique a. Duis congue mi tortor, ut volutpat ante pharetra id. Sed at tincidunt purus. Integer suscipit tortor eros, in blandit justo congue quis. Nullam pellentesque erat nec purus hendrerit, quis commodo ligula tempor. Integer congue consequat nisi, consectetur mattis nulla lobortis eget. Nunc feugiat bibendum est ac laoreet. Vivamus sed pellentesque odio, et placerat orci. Maecenas rhoncus maximus leo efficitur posuere. Proin cursus, turpis vel pretium sollicitudin, ipsum dui laoreet ex, ac blandit sapien dui quis ante. Vestibulum mi ex, venenatis faucibus arcu ut, convallis euismod ipsum. Mauris imperdiet risus ex, eget condimentum sapien finibus et.
-
-
+                            </div>
+                            <div className="post__utils-box">
+                                <div className="post__delete">삭제</div>
+                                <div className="post__edit">수정</div>
                             </div>
 
                         </Link>
@@ -33,9 +39,9 @@ export default function Home() {
                 ))}
             </div>
             <footer>
-                <div>Menu 1</div>
-                <div>Menu 2</div>
-                <div>Menu 3</div>
+                <Link to="/posts/new">글쓰기</Link>
+                <Link to="/posts">게시글</Link>
+                <Link to="/profile">프로필</Link>
             </footer>
         </div>
     )
