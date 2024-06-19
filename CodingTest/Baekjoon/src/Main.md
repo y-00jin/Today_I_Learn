bd10f250fcf205441cde1170a86d8699f3dc2b3d
yjlee <81798918+y-00jin@users.noreply.github.com>
2024-06-19 17:41:41 +0900

    24.06.19 코딩테스트 백준

--- CodingTest/Baekjoon/src/Main.md
+++ CodingTest/Baekjoon/src/Main.md
@@ -1,12 +1,22 @@
 <h1><img src="https://d2gd6pc034wcta.cloudfront.net/tier/1.svg" width="30" height="30" style="vertical-align: middle;"/> [] </h1>
 
-<h3>문제</h3>
+<h3>Question</h3>
+
+<h4>Input</h4>
+
+<h4>Output</h4>
+
+<br><br>
+
+<hr>
 
 <br>
 
-<h3>정답</h3>
+<h3>Answer</h3>
+
 
 💡 
+
 <br>
 
 ```java
--- CodingTest/Baekjoon/src/_01xxx/_1271.java
+++ CodingTest/Baekjoon/src/_01xxx/_1271.java
@@ -1,17 +1,24 @@
 package _01xxx;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class _1271 {
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String inputValues = br.readLine();
-		StringTokenizer st = new StringTokenizer(inputValues);
-		int n = Integer.parseInt(st.nextToken());	// 돈
-		int m = Integer.parseInt(st.nextToken());	// 돈을 받으러 온 생명체 수
-		System.out.println(m == 0 ? "0": n/m);	// 생명체 하나에게 돌아가는 돈의 양
-		System.out.println(m == 0 ? n: n%m);	// 1원씩 분배할 수 없는 남는 돈
-		br.close();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String inputValues = br.readLine();
+        StringTokenizer st = new StringTokenizer(inputValues);
+
+        BigInteger n = new BigInteger(st.nextToken()); // 최백준 조교가 가진 돈
+        BigInteger m = new BigInteger(st.nextToken()); // 돈을 받으러 온 생명체 수
+
+        BigInteger quotient = n.divide(m); // 생명체 하나에게 돌아가는 돈의 양
+        BigInteger remainder = n.remainder(m); // 1원씩 분배할 수 없는 남는 돈
+
+        System.out.println(quotient);
+        System.out.println(remainder);
+
+        br.close();
 	}
 }
