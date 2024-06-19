4a5f4795f13bac1109be6cec36b40a04a6e5929b
yjlee <81798918+y-00jin@users.noreply.github.com>
2024-06-19 14:05:50 +0900

    2024.06.19 백준 코딩테스트

---
+++ CodingTest/Baekjoon/src/_10xxx/_10998.java
@@ -0,0 +1,16 @@
+package _10xxx;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _10998 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String inputValues = br.readLine();
+		StringTokenizer st = new StringTokenizer(inputValues);
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		System.out.println(a * b);
+	}
+}
