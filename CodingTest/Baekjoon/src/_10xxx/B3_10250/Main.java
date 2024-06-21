5878ae78006f061e65c2e2fc5c2a4758d9d7a990
Yoojin <81798918+y-00jin@users.noreply.github.com>
2024-06-22 02:35:00 +0900

    24.06.22 백준 - 10818 최소, 최대

---
+++ CodingTest/Baekjoon/src/_10xxx/B3_10250/Main.java
@@ -0,0 +1,20 @@
+package _10xxx.B3_10250;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCount = Integer.parseInt(br.readLine());
+        for(int i = 0 ; i<testCount;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int height = Integer.parseInt(st.nextToken());
+            int width = Integer.parseInt(st.nextToken());
+            int guest = Integer.parseInt(st.nextToken());
+
+
+        }
+    }
+}
