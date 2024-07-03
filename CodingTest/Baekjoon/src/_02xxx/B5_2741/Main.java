b29688c4729ecc1dc6d59ee975861c229c355b63
yjlee <81798918+y-00jin@users.noreply.github.com>
2024-07-03 10:16:15 +0900

    [백준] CLASS 1++

---
+++ CodingTest/Baekjoon/src/_02xxx/B5_2741/Main.java
@@ -0,0 +1,22 @@
+package _02xxx.B5_2741;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        for(int i=1;i<=N;i++){
+            bw.write(i + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
