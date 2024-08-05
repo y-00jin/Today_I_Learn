6f9f3ebfdab1397df5c55fb081427dcba4f8bf1f
yjlee <81798918+y-00jin@users.noreply.github.com>
2024-08-05 10:24:31 +0900

    [백준] 31403 A+B-C

--- CodingTest/Baekjoon/src/_30xxx/_31403_B4/Main.java
+++ CodingTest/Baekjoon/src/_30xxx/_31403_B4/Main.java
@@ -6,6 +6,11 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuffer sb = new StringBuffer();
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+
+        System.out.println(A + B - C);
+        System.out.println( Integer.parseInt(A + "" + B) - C);
     }
 }
