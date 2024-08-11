19a3c5fce84f010b4d57ee2d7bc5c642fa655a85
Yoojin <81798918+y-00jin@users.noreply.github.com>
2024-08-12 01:42:40 +0900

    [백준] 2558 - A+B

---
+++ CodingTest/Baekjoon/src/_02xxx/_2558_B5/Main.java
@@ -0,0 +1,10 @@
+package _02xxx._2558_B5;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        System.out.println(A+B);
+    }
+}
