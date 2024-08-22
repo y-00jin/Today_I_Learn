18ab29503b2e71d3cc4134f8c7f54be199ff78fa
yjlee <81798918+y-00jin@users.noreply.github.com>
2024-08-22 10:14:43 +0900

    [백준] 2775 - 부녀회장이 될테야

--- CodingTest/Baekjoon/src/_02xxx/_2775_B1/Main.java
+++ CodingTest/Baekjoon/src/_02xxx/_2775_B1/Main.java
@@ -5,17 +5,28 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int[][] arr = new int[15][15];
-        for(int i = 0; i < 15; i++) {
-            arr[i][1] = 1;	// i층 1호
-            arr[0][i] = i;	// 0층 i호
+        for (int i = 0; i < 15; i++) {
+            arr[i][1] = 1;    // i층 1호
+            arr[0][i] = i;    // 0층 i호
         }
 
-
+        for (int i = 1; i < 15; i++) {
+            for (int j = 1; j < 15; j++) {
+                arr[i][j] = arr[i][j - 1] + arr[i - 1][j];
+            }
+        }
 
         int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+            bw.write(arr[k][n] + "\n");
+        }
 
-
+        bw.flush();
+        bw.close();
     }
 }
