8986fb5c3a113e8770b59fb0c1759139d78dc78e
Yoojin <81798918+y-00jin@users.noreply.github.com>
2024-08-24 23:42:20 +0900

    [백준] 9655_S5

---
+++ CodingTest/Baekjoon/src/_09xxx/_9655_S5/Main.java
@@ -0,0 +1,13 @@
+package _09xxx._9655_S5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(N % 2 == 1 ? "SK" : "CY");
+    }
+}
