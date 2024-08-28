a0caa75b838d2f02ee5f1ceade1cfa1cf3149860
Yoojin <81798918+y-00jin@users.noreply.github.com>
2024-08-28 19:57:24 +0900

    [백준] 1546 - 평균

--- CodingTest/Baekjoon/src/_01xxx/_1546_B1/Main.java
+++ CodingTest/Baekjoon/src/_01xxx/_1546_B1/Main.java
@@ -1,22 +1,20 @@
 package _01xxx._1546_B1;
 
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        double[] scores = new double[Integer.parseInt(br.readLine())];
-
+        int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0;i<scores.length;i++){
-            scores[i] = Double.parseDouble(st.nextToken());
+        long M = 0;
+        long sum = 0;
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            M = num > M ? num : M;
+            sum += num;
         }
-
-        double result = 0;
-        for(double score : scores){
-            result += score / Arrays.stream(scores).max().getAsDouble() * 100;
-        }
-        System.out.println(result / scores.length);
+        System.out.println(sum * 100.0 / M / N);
     }
 }
