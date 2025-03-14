b7582824f3e4ecdc06c906eabb7a35a284c0b3aa
yjlee <edbwls2@naver.com>
2025-03-14 11:19:55 +0900

    [백준] 1000 - A+B

--- CodingTest/백준/_01xxx/_1000_AB더하기_B5/Main.java
+++ CodingTest/백준/_01xxx/_1000_AB더하기_B5/Main.java
@@ -1,7 +1,24 @@
 package _01xxx._1000_AB더하기_B5;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!!!!");
+    public static void main(String[] args) throws Exception {
+
+        // # BufferedReader 사용 -> 64초
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	// INPUT
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        System.out.println(a + b);
+        br.close();
+
+        // # Scanner 사용 -> 112초
+//		Scanner sc = new Scanner(System.in);
+//		int a = sc.nextInt();
+//		int b = sc.nextInt();
+//		System.out.println(a+b);
     }
 }
