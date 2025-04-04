9eb8d557d2bc383b24d9fc8e0dd365982270f70b
yjlee <edbwls2@naver.com>
2025-04-04 10:45:57 +0900

    [백준] 2018 - 수들의 합 5

--- CodingTest/백준/_02xxx/_2018_S5_수들의합5/Main.java
+++ CodingTest/백준/_02xxx/_2018_S5_수들의합5/Main.java
@@ -1,2 +1,29 @@
-package _02xxx._2018_S5_수들의합5;public class Main {
+package _02xxx._2018_S5_수들의합5;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int startIndex = 1;
+        int endIndex = 1;
+        int count = 1;
+        int sum = 1;
+        while (endIndex != n){
+            if(sum == n){
+                count++;
+                endIndex++;
+                sum += endIndex;
+            } else if(sum < n){
+                endIndex++;
+                sum += endIndex;
+            } else{
+                sum -= startIndex;
+                startIndex++;
+            }
+        }
+        System.out.println(count);
+    }
 }
