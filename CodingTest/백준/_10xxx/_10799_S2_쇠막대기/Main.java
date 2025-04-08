0250c5d798e0ead09dcb2192e8b913ac76a61faf
yjlee <edbwls2@naver.com>
2025-04-08 11:26:41 +0900

    [백준] 10799 - 쇠 막대기

--- CodingTest/백준/_10xxx/_10799_S2_쇠막대기/Main.java
+++ CodingTest/백준/_10xxx/_10799_S2_쇠막대기/Main.java
@@ -17,12 +17,10 @@ public class Main {
                 stack.push(ch);
             } else {
                 stack.pop();
-
                 if (str.charAt(i - 1) == '(') { // 레이저 -> 현재 스택에 남은 막대기 수만큼 잘림
                     result += stack.size();
-                } else {    // 막대기 끝 -> ))
-                    // 막대기 끝 -> 잘린 조각 +1
-                    result += 1;
+                } else {
+                    result += 1;    // 막대기 끝 )) -> 잘린 조각 +1
                 }
             }
         }
