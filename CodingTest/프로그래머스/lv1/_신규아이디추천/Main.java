2e889b3e9d72cd5f80d71b66546315b773323ce0
yjlee <edbwls2@naver.com>
2025-04-21 10:21:41 +0900

    [프로그래머스] 기본 틀

---
+++ CodingTest/프로그래머스/lv1/_신규아이디추천/Main.java
@@ -0,0 +1,2 @@
+package lv1._신규아이디추천;public class Main {
+}
---
+++ CodingTest/프로그래머스/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Solution s = new Solution();
+    }
+}
+
+class Solution {
+    public String solution(String new_id) {
+        String answer = "";
+        return answer;
+    }
+}
