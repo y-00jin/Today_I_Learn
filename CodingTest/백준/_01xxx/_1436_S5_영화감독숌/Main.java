218f004534c06220588683d66d4e57c897879abc
yjlee <edbwls2@naver.com>
2025-09-24 15:10:26 +0900

    [백준] 1436 - 영화감독 숌

---
+++ CodingTest/백준/_01xxx/_1436_S5_영화감독숌/Main.java
@@ -0,0 +1,22 @@
+package _01xxx._1436_S5_영화감독숌;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int val = 666;
+        int count = 1;
+        while(N != count){
+            val++;
+            if(String.valueOf(val).contains("666")){
+                count++;
+            }
+        }
+        System.out.println(val);
+    }
+}
