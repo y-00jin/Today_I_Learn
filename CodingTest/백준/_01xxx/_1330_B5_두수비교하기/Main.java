218a85a6703de2893cd15e0eb7c0b9fa84bac360
yjlee <edbwls2@naver.com>
2025-09-24 14:40:26 +0900

    [백준] 1330 - 두 수 비교하기

---
+++ CodingTest/백준/_01xxx/_1330_B5_두수비교하기/Main.java
@@ -0,0 +1,14 @@
+package _01xxx._1330_B5_두수비교하기;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        System.out.println( A > B ? ">" : (A < B ? "<" : "==") );
+    }
+}
